17546622f4ebbb5a9083e266943cbac04e4100d9
Marjona Rakhmatillaeva <70479211+Marjonagit@users.noreply.github.com>
2023-11-20 22:34:43 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:hw3/screens/myscreen.dart';
-import 'package:hw3/routes.dart';
+import 'package:provider/provider.dart';
+import 'screen1.dart';
+import 'screen2.dart';
+import 'screen3.dart';
+import 'screen4.dart';
+import 'screen5.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class DataProvider extends ChangeNotifier {
+  String message = '';
+
+  void updateMessage(String newMessage) {
+    message = newMessage;
+    notifyListeners();
+  }
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Marjon App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => DataProvider(),
+      child: MaterialApp(
+        title: 'HW3',
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomeScreen(),
+          '/screen1': (context) => Screen1(),
+          '/screen2': (context) => Screen2(),
+          '/screen3': (context) => Screen3(),
+          '/screen4': (context) => Screen4(),
+          '/screen5': (context) => Screen5(),
+        },
       ),
-      initialRoute: '/my',
-      routes: routes,
-      home: const MyScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/screen1');
+          },
+          child: Text('Go to Screen 1'),
+        ),
+      ),
     );
   }
 }
