1b3301806a67e953887eb925ac97cb2328bffbdd
Marjona Rakhmatillaeva <70479211+Marjonagit@users.noreply.github.com>
2023-11-20 23:09:03 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,51 +10,12 @@ void main() {
   runApp(MyApp());
 }
 
-class DataProvider extends ChangeNotifier {
-  String message = '';
-
-  void updateMessage(String newMessage) {
-    message = newMessage;
-    notifyListeners();
-  }
-}
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => DataProvider(),
-      child: MaterialApp(
-        title: 'HW3',
-        initialRoute: '/',
-        routes: {
-          '/': (context) => HomeScreen(),
-          '/screen1': (context) => Screen1(),
-          '/screen2': (context) => Screen2(),
-          '/screen3': (context) => Screen3(),
-          '/screen4': (context) => Screen4(),
-          '/screen5': (context) => Screen5(),
-        },
-      ),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Screen'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/screen1');
-          },
-          child: Text('Go to Screen 1'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'MarjonApp',
+      home: Screens(),
     );
   }
 }
